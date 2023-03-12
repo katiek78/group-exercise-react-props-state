b44b24601dc4c8362c8c136ce78c9f8f579341eb
katiek78 <kermodekatie@gmail.com>
2023-03-12 20:44:19 +0000

    Pass props to ChuckInfo to display information

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -14,7 +14,7 @@ const ChuckCard: React.FC<ChuckCardProps> = ({greeting}) => {
 			{/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
 			<h2>{ greeting }</h2>
 
-			<img src={'Chuck image here!'} alt='A handsome man' />
+			<img src={ChuckImage} alt='A handsome man' />
 		</>
 	)
 }
